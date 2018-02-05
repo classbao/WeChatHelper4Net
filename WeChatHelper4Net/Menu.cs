232bd3ba8d43116d2401aaf98062bda277d84b6c
熊仔其人 <xiongzaiqiren@163.com>
2018-02-05 11:58:16 +0800

    获取自定义菜单配置

--- WeChatHelper4Net/Menu.cs
+++ WeChatHelper4Net/Menu.cs
@@ -10,50 +10,50 @@ using WeChatHelper4Net.Models;
  */
 namespace WeChatHelper4Net
 {
-	/// <summary>
-	/// 自定义菜单
-	/// </summary>
-	public class Menu
-	{
-		/// <summary>
-		/// 查询当前使用的自定义菜单的结构
-		/// </summary>
-		/// <returns></returns>
-		public static string GetMenu(DateTime now)
-		{
-			string url = Common.ApiUrl + string.Format("menu/get?access_token={0}", AccessToken.GetAccessToken(now));
-			return HttpRequestHelper.Request(url);
-		}
+    /// <summary>
+    /// 自定义菜单
+    /// </summary>
+    public class Menu
+    {
+        /// <summary>
+        /// 查询当前使用的自定义菜单的结构
+        /// </summary>
+        /// <returns></returns>
+        public static string GetMenu(DateTime now)
+        {
+            string url = Common.ApiUrl + string.Format("menu/get?access_token={0}", AccessToken.GetAccessToken(now));
+            return HttpRequestHelper.Request(url);
+        }
 
-		/// <summary>
-		/// 删除当前使用的自定义菜单
-		/// </summary>
-		/// <returns></returns>
-		public static string DeleteMenu(DateTime now)
-		{
-			string url = Common.ApiUrl + string.Format("menu/delete?access_token={0}", AccessToken.GetAccessToken(now));
-			return HttpRequestHelper.Request(url);
-		}
+        /// <summary>
+        /// 删除当前使用的自定义菜单
+        /// </summary>
+        /// <returns></returns>
+        public static string DeleteMenu(DateTime now)
+        {
+            string url = Common.ApiUrl + string.Format("menu/delete?access_token={0}", AccessToken.GetAccessToken(now));
+            return HttpRequestHelper.Request(url);
+        }
 
-		/// <summary>
-		/// 创建自定义菜单
-		/// </summary>
-		/// <param name="data">创建自定义菜单JSON结构字符串</param>
-		/// <returns></returns>
-		public static bool CreateMenu(string data, DateTime now)
-		{
-			if (string.IsNullOrEmpty(data)) return false;
-			try
-			{
-				string url = Common.ApiUrl + string.Format("menu/create?access_token={0}", AccessToken.GetAccessToken(now));
+        /// <summary>
+        /// 创建自定义菜单
+        /// </summary>
+        /// <param name="data">创建自定义菜单JSON结构字符串</param>
+        /// <returns></returns>
+        public static bool CreateMenu(string data, DateTime now)
+        {
+            if(string.IsNullOrEmpty(data)) return false;
+            try
+            {
+                string url = Common.ApiUrl + string.Format("menu/create?access_token={0}", AccessToken.GetAccessToken(now));
 
-				string result = HttpRequestHelper.Request(url, data, HttpRequestHelper.Method.POST, System.Text.Encoding.UTF8);
-                if (Common.ReturnJSONisOK(result))
+                string result = HttpRequestHelper.Request(url, data, HttpRequestHelper.Method.POST, System.Text.Encoding.UTF8);
+                if(Common.ReturnJSONisOK(result))
                     return true;
                 else
                 {
                     RequestResultBaseModel model = JsonHelper.DeSerialize<RequestResultBaseModel>(result);
-                    if (model != null && !string.IsNullOrEmpty(model.errmsg) && model.errcode == 0)
+                    if(model != null && !string.IsNullOrEmpty(model.errmsg) && model.errcode == 0)
                     {
                         return true;
                     }
@@ -63,13 +63,24 @@ namespace WeChatHelper4Net
                         return false;
                     }
                 }
-			}
-			catch (Exception Ex)
-			{
-				LogHelper.Save(Ex);
-				throw Ex;
-			}
-		}
+            }
+            catch(Exception Ex)
+            {
+                LogHelper.Save(Ex);
+                throw Ex;
+            }
+        }
 
-	}
+        /// <summary>
+        /// 获取自定义菜单配置
+        /// </summary>
+        /// <param name="now"></param>
+        /// <returns></returns>
+        public static string GetCurrentSelfMenu(DateTime now)
+        {
+            string url = Common.ApiUrl + string.Format("get_current_selfmenu_info?access_token={0}", AccessToken.GetAccessToken(now));
+            return HttpRequestHelper.Request(url);
+        }
+
+    }
 }
