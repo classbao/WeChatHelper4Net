abbd8b256f703dae97852a7121cb4cd5c27ae6d7
熊仔其人 <xiongzaiqiren@163.com>
2018-02-23 15:47:58 +0800

    微信JS-SDK，授权参数，允许分享以及不允许分享部分试图代码

---
+++ SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 允许微信分享的页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AllowWeChatToShare()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 不允许微信分享的页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NotAllowWeChatToShare()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SampleWebApp/Controllers/SharedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
