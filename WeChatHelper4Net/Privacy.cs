d417258afad24c34825df534e29ceacaeb1f8653
熊仔其人 <xiongzaiqiren@163.com>
2021-04-06 22:13:48 +0800

    微信支付信息注释

--- WeChatHelper4Net/Privacy.cs
+++ WeChatHelper4Net/Privacy.cs
@@ -18,15 +18,18 @@ namespace WeChatHelper4Net
         #region 微信商户&财付通账号
         /// <summary>
         /// 商户号（财付通商户身份标识 MCHID）
+        /// https://pay.weixin.qq.com/wiki/doc/api/H5.php?chapter=9_20&index=1
         /// </summary>
         public static readonly string PartnerID = ConfigHelper.GetAppSetting("WeChatPartnerID"); //"12***01";
         /// <summary>
         /// 财付通商户权限密钥（API密钥）
+        /// https://pay.weixin.qq.com/wiki/doc/api/H5.php?chapter=4_3
         /// </summary>
         public static readonly string PartnerKey = ConfigHelper.GetAppSetting("WeChatPartnerKey"); //"91013efa57330c52680016f602ff912c";
 
         /// <summary>
         /// 微信公众号支付请求中用于加密的秘钥paySignKey，对应于支付场景中的AppKey
+        /// https://pay.weixin.qq.com/wiki/doc/api/jsapi_sl.php?chapter=7_7&index=6
         /// </summary>
         public static readonly string AppKey = ConfigHelper.GetAppSetting("WeChatpaySignKey"); //"hn***FM";
 
